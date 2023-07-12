91b0e10456e66e8d979ab72d40f98ef7f7ee83ba
Afolabi Ayanfe <ayanfesolutions@gmail.com>
2023-07-12 11:57:39 +0100

    Add project files.

---
+++ JokesApp/Models/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace JokesApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
+
